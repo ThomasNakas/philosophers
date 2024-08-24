b4cbad4badea51594b19276437df273ca0b3f480
Thomas Nakas <tnakas@2-E-2.42heilbronn.de>
2024-08-24 12:24:28 +0200

    extra debugging on the big_sixty_ms

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 13:02:37 by tnakas            #+#    #+#             */
-/*   Updated: 2024/08/23 19:49:04 by tnakas           ###   ########.fr       */
+/*   Updated: 2024/08/24 12:22:51 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(int argc, char **argv)
 {
 	t_table			table;
 	t_supervisor	s;
-	(void)(argc);
+
 	init_supervisor(argc, argv, &s);
 	
 	return (printf("%d\n", (int)ft_p_atol(argv[1])));
